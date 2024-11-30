6375156515febe0233d096d375851adcd570e187
czgaotian <811460602@qq.com>
2024-11-30 20:31:47 +0100

    init

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,25 @@
+import * as clientConfig0 from '/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.61_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@_qmneunmmxkm7hio2ayq4pzxx7a/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from '/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.61_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc_eznoveqzvc6r6rro4eid2yvfei/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from '/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.61_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc.7_yo3h5ioxwuhpnum2enwr32ayxq/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from '/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-markdown-hint@2.0.0-rc.61_markdown-it@14.1.0_vue@3.5.13_vuepress@2.0.0-rc.18_ygzbyr5p5kg7fxpzvpmmr4dmdq/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from '/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.61_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc_wh2uiy4yzxxwspt35vdk6obkma/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig5 from '/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.61_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc.7_acantopzomepoxgbnmtfcy7oti/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig6 from '/home/gaotian/Projects/learn-English/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig7 from '/home/gaotian/Projects/learn-English/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig8 from '/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.61_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc._al6tfozr2nqi35wgltbsbzcqfa/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig9 from '/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.61_markdown-it@14.1.0_sass@1.81.0_vuepress@2.0.0-rc.18_@vuep_ued5qatfvsjwmztk7idqj7xox4/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig10 from '/home/gaotian/Projects/learn-English/docs/.vuepress/client.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+  clientConfig10,
+].map((m) => m.default).filter(Boolean)
---
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -0,0 +1,10 @@
+import { CodeTabs } from "/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.61_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syjr5cr7ok7os674wmjtki3iue/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.61_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syjr5cr7ok7os674wmjtki3iue/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "/home/gaotian/Projects/learn-English/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.61_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syjr5cr7ok7os674wmjtki3iue/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+
+export default {
+  enhance: ({ app }) => {
+    app.component("CodeTabs", CodeTabs);
+    app.component("Tabs", Tabs);
+  },
+};
